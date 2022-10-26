bf06fb627d90ee900207478812b4204d660abee3
Rufus Pollock <rufus.pollock@datopian.com>
2022-10-26 08:33:34 +0200

    [site,bugfix,config][s]: fix breaking build plus do some configuration.
    * correctly symlink assets folder (and add some content - maybe being empty was an issue) so that build works
    * config: add logo now we have that in assets plus title and description
    * Also add fix to flowershow where siteConfig.js import of user config was inconsistent with docs

--- site/content/config.js
+++ site/content/config.js
@@ -1,1 +1,14 @@
-{}
+const userConfig = {
+  title: "Life Itself",
+  description: "We’re committed to practical action for a radically wiser, weller world. We create hubs, start businesses, do research and engage in activism to pioneer a wiser culture.",
+  author: "Life Itself",
+  navbarTitle: {
+    logo: "/assets/site/life-itself-logo.svg",
+    text: "Life Itself",
+  },
+  navLinks: [
+    { href: "/about", name: "About" }
+  ],
+};
+
+export default userConfig;
