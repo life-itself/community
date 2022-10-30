052530ad47720bc823afc6511f66e02ee99e29f5
Rufus Pollock <rufus.pollock@datopian.com>
2022-10-30 13:30:18 +0100

    [site/config][s]: enable google analytics - refs #170.

--- site/content/config.js
+++ site/content/config.js
@@ -2,6 +2,8 @@ const userConfig = {
   title: "Life Itself",
   description: "We’re committed to practical action for a radically wiser, weller world. We create hubs, start businesses, do research and engage in activism to pioneer a wiser culture.",
   author: "Life Itself",
+  // Google analytics key e.g. G-XXXX
+  analytics: "G-5YH3R887BV",
   navbarTitle: {
     logo: "/assets/site/life-itself-logo.svg",
     text: "Life Itself",
