5722a4ce7fc261df5a05339ee6ae78136300b3de
khalilcodes <khalilali.j@gmail.com>
2022-12-22 23:20:03 +0400

    [layouts,#184][s]: add blog and people layouts

---
+++ site/layouts/people.jsx
@@ -0,0 +1,22 @@
+/* eslint import/no-default-export: off */
+export default function PeopleLayout({ children, frontMatter }) {
+  const { name, avatar } = frontMatter;
+
+  return (
+    <article className="prose text-primary dark:text-primary-dark dark:prose-invert prose-headings:font-headings prose-a:break-words w-full mx-auto p-6">
+      <header>
+        <div className="flex items-center space-x-6">
+          {avatar && <div className="w-48">
+            <img className="rounded-lg object-cover shadow-lg" src={avatar} alt={name} />
+          </div>}
+          <div className="flex flex-col">
+            {name && name.split(" ").map(n => (
+              <h1 key={n} className="even:text-primary odd:text-secondary even:text-5xl odd:text-6xl m-0 odd:ml-4">{n}</h1>
+            ))}
+          </div>
+        </div>
+      </header>
+      <section>{children}</section>
+    </article>
+  );
+}
