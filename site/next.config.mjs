1c40664bd7f916587f03258b3c75ccca953d12e0
khalilcodes <khalilali.j@gmail.com>
2023-01-14 19:12:02 +0300

    [site,#259][s]: add page redirects

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,6 +1,52 @@
 import { withContentlayer } from "next-contentlayer";
 
+const redirects = [
+  {
+    source: '/manifesto',
+    destination: '/blog/2015/11/01/manifesto',
+    permanent: true,
+  },
+  {
+    source: '/27aout',
+    destination: '/blog/2022/07/15/art-eco-spirituality-aug-2022',
+    permanent: true,
+  },
+  {
+    source: '/sunflower-school',
+    destination: '/blog/2021/11/12/sunflower-school-ecole-des-tournesols',
+    permanent: true,
+  },
+  {
+    source: '/mindfulness',
+    destination: '/blog/2020/06/29/mindfulness-an-introduction',
+    permanent: true,
+  },
+  {
+    source: '/bergerac-build',
+    destination: '/blog/2020/07/09/bergerac-build-festival-2020-gathering-builders-diggers-and-dreamers',
+    permanent: true,
+  },
+  {
+    source: '/imaginary-society',
+    destination: '/blog/2021/09/09/the-imaginary-society-series',
+    permanent: true,
+  },
+  {
+    source: '/institute/compassionate-mental-health',
+    destination: '/blog/2021/05/14/compassionate-mental-health/',
+    permanent: true,
+  },
+  {
+    source: '/community-projects',
+    destination: '/blog/2021/03/18/berlin-hub-community-projects',
+    permanent: true,
+  }
+]
+
 export default withContentlayer({
+  async redirects() {
+    return redirects
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
