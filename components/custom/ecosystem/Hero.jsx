350b497a9ef2bb5be65e18932c8ee7225b2e0028
Rufus Pollock <rufus.pollock@datopian.com>
2023-05-11 13:03:43 +0200

    [ecosystem][xs]: tweak main title.

--- components/custom/ecosystem/Hero.jsx
+++ components/custom/ecosystem/Hero.jsx
@@ -80,7 +80,7 @@ export default function Hero() {
 					<div className="text-center">
 						<h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
 							{/* <span className="block xl:inline">{config.title}</span> */}
-							<span className="block xl:inline">Polycrisis Response Mapping</span>
+							<span className="block xl:inline">Mapping an Emerging Ecosystem</span>
 						</h1>
 						<h2 className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
 							{/* {config.tagline} */}
