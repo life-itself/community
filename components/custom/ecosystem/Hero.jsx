3cefbed216556da586dd677c3e2170fd2cfd8f4d
khalilcodes <khalilali.j@gmail.com>
2023-05-20 20:01:00 +0300

    [ecosystem,components,#448][s]: add new hero titles

--- components/custom/ecosystem/Hero.jsx
+++ components/custom/ecosystem/Hero.jsx
@@ -7,11 +7,14 @@ export default function Hero() {
   useEffect(() => {
     const typed = new Typed(el.current, {
       strings: [
-        "Mapping an<br class='sm:hidden'/> Emerging Ecosystem",
-        "Metamodern<br class='sm:hidden'/> Ecosystem"
+        "Metamodern Ecosystem",
+        "Polycrisis Ecosystem",
+        "Metacrisis Ecosystem",
+        "Great Transition Ecosystem",
+        "Liminal Ecosystem"
       ],
       typeSpeed: 45,
-      backDelay: 1500,
+      backDelay: 1650,
       showCursor: false,
       fadeOut: true,
       fadeOutDelay: 350,
@@ -99,8 +102,9 @@ export default function Hero() {
 			<div className="relative pt-6 pb-16 sm:pb-24">
 				<main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
 					<div className="text-center">
-						<h1 className="h-28 lg:h-14 text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-							<span ref={el} className="mx-auto w-fit min-[375px]:min-w-max sm:w-auto flex sm:inline" />
+						<h1 className="flex flex-col gap-y-2 h-48 min-[391px]:h-32 sm:h-48 lg:h-32 text-4xl tracking-tight font-extrabold text-primary sm:text-5xl md:text-6xl">
+							<span>Mapping an Emerging</span>
+              <span ref={el} className="mx-auto w-fit sm:w-auto flex sm:inline" />
 						</h1>
 						<h2 className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
 							{/* {config.tagline} */}
