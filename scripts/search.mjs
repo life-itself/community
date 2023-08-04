3e554fb7f490eb3eea4dace320bdcf7a01705f8c
Ola Rubaj <52197250+olayway@users.noreply.github.com>
2023-08-04 19:07:29 +0200

    [scripts/search][s]: upgrade script to use mddb instead of contentlayer

--- scripts/search.mjs
+++ scripts/search.mjs
@@ -1,25 +1,40 @@
 import { writeFileSync } from "fs";
-import { allDocuments } from "../.contentlayer/generated/index.mjs";
 
-const omit = (obj = {}, keys = []) => {
-  const result = Object.assign({}, obj);
-  keys.forEach((key) => {
-    delete result[key];
-  });
-  return result;
-};
+import config from "../content/config.mjs";
+import clientPromise from "../lib/mddb.mjs";
 
-const coreContent = allDocuments
-  .map((doc) =>
-    omit(
-      {
+// const omit = (obj = {}, keys = []) => {
+//   const result = Object.assign({}, obj);
+//   keys.forEach((key) => {
+//     delete result[key];
+//   });
+//   return result;
+// };
+
+export default async function search() {
+  const mddb = await clientPromise;
+  const allFiles = await mddb.getFiles({ extensions: ["md", "mdx"] });
+  const coreContent = allFiles
+    .filter((doc) => !doc.metadata?.isDraft)
+    .map(
+      (doc) => ({
         ...doc,
-        sourceDir:
-          doc._raw.sourceFileDir === "." ? null : doc._raw.sourceFileDir,
-      },
-      ["body", "_raw", "_id"]
-    )
-  )
-  .filter((doc) => !doc.isDraft);
+        sourceDir: doc.file_path,
+        urlPath: doc.url_path,
+      })
+      // TODO old contentlayer related code
+      // omit(
+      //   {
+      //     ...doc,
+      //     sourceDir: doc.file_path
+      //   },
+      //   ["body", "_raw", "_id"]
+      // )
+    );
 
-writeFileSync("public/search.json", JSON.stringify(coreContent));
+  if (config?.search?.provider === "kbar") {
+    writeFileSync("public/search.json", JSON.stringify(coreContent));
+    console.log("Local search index generated...");
+  }
+  return;
+}
