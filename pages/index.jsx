0ac875f9d28ec7c080bd39505b4a39348f10cb0a
nathenf <95729108+nathenf@users.noreply.github.com>
2023-11-10 05:54:51 +0100

    Update index.jsx
    - Replaced images for learn pages
    - Added Cultural Evolution Learn Page

--- pages/index.jsx
+++ pages/index.jsx
@@ -255,21 +255,21 @@ export async function getStaticProps() {
   const learnTopics = [
     {
       id: 1,
-      title: "Web3",
-      link: "http://web3.lifeitself.org",
-      image: "assets/images/web3.jpeg"
+      title: "Fundamental wellbeing",
+      link: "https://lifeitself.org/learn/fundamental-wellbeing",
+      image: "assets/images/fundamental-wellbeing-learn-homepage.jpg"
     },
     {
       id: 2,
-      title: "Ecosystem",
-      link: "https://lifeitself.org/ecosystem",
-      image: "images/assets/ecosystem-project-image.png"
+      title: "Cultural Evolution",
+      link: "https://lifeitself.org/learn/culturology",
+      image: "images/assets/cultural-evolution-learn-homepage.jpg"
     },
     {
       id: 3,
-      title: "Fundamental wellbeing",
-      link: "https://lifeitself.org/learn/fundamental-wellbeing",
-      image: "assets/images/fundamental-wellbeing-dr-jeffery-martin.jpg"
+      title: "Web3",
+      link: "http://web3.lifeitself.org",
+      image: "assets/images/web3-learn-homepage.jpg"
     },
   ]
 
