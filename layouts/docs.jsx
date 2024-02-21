d80be4f4eb891fa479302a94424721977e48665c
Rufus Pollock <rufus.pollock@datopian.com>
2024-02-21 23:01:25 +0100

    [layouts/docs][s]: remove showing the created date at the top of pages.

--- layouts/docs.jsx
+++ layouts/docs.jsx
@@ -7,11 +7,6 @@ export default function DocsLayout({ children, frontMatter }) {
     <article className="docs prose text-primary dark:text-primary-dark dark:prose-invert prose-headings:font-headings prose-a:break-words mx-auto p-6">
       <header>
         <div className="mb-6">
-          {created && (
-            <p className="text-sm text-zinc-400 dark:text-zinc-500">
-              <time dateTime={created}>{formatDate(created)}</time>
-            </p>
-          )}
           {title && <h1>{title}</h1>}
         </div>
       </header>
